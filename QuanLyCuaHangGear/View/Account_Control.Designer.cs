9ff276fb2e14b9726248170d2f017b090bc1d34b
Quoc Dat <ngoluuquocdat@gmail.com>
2020-06-06 11:39:04 +0700

    Add project files.

---
+++ QuanLyCuaHangGear/View/Account_Control.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace QuanLyCuaHangGear.View
+{
+    partial class Account_Control
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_RepeatNewPass = new System.Windows.Forms.TextBox();
+            this.txt_NewPass = new System.Windows.Forms.TextBox();
+            this.txt_CurrentPass = new System.Windows.Forms.TextBox();
+            this.txt_DisplayName = new System.Windows.Forms.TextBox();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.pic_User = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_User)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(592, 513);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 1);
+            this.panel1.TabIndex = 159;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.ForeColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(592, 461);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(310, 1);
+            this.panel2.TabIndex = 160;
+            // 
+            // txt_RepeatNewPass
+            // 
+            this.txt_RepeatNewPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.txt_RepeatNewPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_RepeatNewPass.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_RepeatNewPass.ForeColor = System.Drawing.Color.White;
+            this.txt_RepeatNewPass.Location = new System.Drawing.Point(592, 589);
+            this.txt_RepeatNewPass.Name = "txt_RepeatNewPass";
+            this.txt_RepeatNewPass.ReadOnly = true;
+            this.txt_RepeatNewPass.Size = new System.Drawing.Size(310, 27);
+            this.txt_RepeatNewPass.TabIndex = 148;
+            this.txt_RepeatNewPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_NewPass
+            // 
+            this.txt_NewPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.txt_NewPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_NewPass.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_NewPass.ForeColor = System.Drawing.Color.White;
+            this.txt_NewPass.Location = new System.Drawing.Point(592, 539);
+            this.txt_NewPass.Name = "txt_NewPass";
+            this.txt_NewPass.ReadOnly = true;
+            this.txt_NewPass.Size = new System.Drawing.Size(310, 27);
+            this.txt_NewPass.TabIndex = 149;
+            this.txt_NewPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_CurrentPass
+            // 
+            this.txt_CurrentPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.txt_CurrentPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_CurrentPass.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_CurrentPass.ForeColor = System.Drawing.Color.White;
+            this.txt_CurrentPass.Location = new System.Drawing.Point(592, 487);
+            this.txt_CurrentPass.Name = "txt_CurrentPass";
+            this.txt_CurrentPass.ReadOnly = true;
+            this.txt_CurrentPass.Size = new System.Drawing.Size(310, 27);
+            this.txt_CurrentPass.TabIndex = 150;
+            this.txt_CurrentPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_DisplayName
+            // 
+            this.txt_DisplayName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.txt_DisplayName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_DisplayName.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_DisplayName.ForeColor = System.Drawing.Color.White;
+            this.txt_DisplayName.Location = new System.Drawing.Point(592, 432);
+            this.txt_DisplayName.Name = "txt_DisplayName";
+            this.txt_DisplayName.ReadOnly = true;
+            this.txt_DisplayName.Size = new System.Drawing.Size(310, 27);
+            this.txt_DisplayName.TabIndex = 151;
+            this.txt_DisplayName.Text = "Hồ Văn An";
+            this.txt_DisplayName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.txt_UserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_UserName.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_UserName.ForeColor = System.Drawing.Color.White;
+            this.txt_UserName.Location = new System.Drawing.Point(592, 383);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.ReadOnly = true;
+            this.txt_UserName.Size = new System.Drawing.Size(310, 27);
+            this.txt_UserName.TabIndex = 152;
+            this.txt_UserName.Text = "hovanan";
+            this.txt_UserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(214)))), ((int)(((byte)(247)))));
+            this.label3.Location = new System.Drawing.Point(277, 588);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 29);
+            this.label3.TabIndex = 155;
+            this.label3.Text = "Xác nhận mật khẩu mới:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(214)))), ((int)(((byte)(247)))));
+            this.label2.Location = new System.Drawing.Point(277, 538);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 29);
+            this.label2.TabIndex = 156;
+            this.label2.Text = "Mật khẩu mới:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(214)))), ((int)(((byte)(247)))));
+            this.label1.Location = new System.Drawing.Point(277, 486);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 29);
+            this.label1.TabIndex = 157;
+            this.label1.Text = "Mật khẩu cũ:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Transparent;
+            this.label16.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(214)))), ((int)(((byte)(247)))));
+            this.label16.Location = new System.Drawing.Point(277, 433);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(157, 29);
+            this.label16.TabIndex = 154;
+            this.label16.Text = "Tên hiển thị:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(214)))), ((int)(((byte)(247)))));
+            this.label15.Location = new System.Drawing.Point(277, 382);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(176, 29);
+            this.label15.TabIndex = 153;
+            this.label15.Text = "Tên tài khoản:";
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.btn_Edit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.btn_Edit.FlatAppearance.BorderSize = 0;
+            this.btn_Edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Edit.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Edit.ForeColor = System.Drawing.Color.White;
+            this.btn_Edit.Image = global::QuanLyCuaHangGear.Properties.Resources.edit;
+            this.btn_Edit.Location = new System.Drawing.Point(919, 422);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(54, 52);
+            this.btn_Edit.TabIndex = 158;
+            this.btn_Edit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Edit.UseVisualStyleBackColor = false;
+            // 
+            // pic_User
+            // 
+            this.pic_User.Location = new System.Drawing.Point(509, 83);
+            this.pic_User.Name = "pic_User";
+            this.pic_User.Size = new System.Drawing.Size(250, 250);
+            this.pic_User.TabIndex = 147;
+            this.pic_User.TabStop = false;
+            // 
+            // Account_Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.txt_RepeatNewPass);
+            this.Controls.Add(this.txt_NewPass);
+            this.Controls.Add(this.txt_CurrentPass);
+            this.Controls.Add(this.txt_DisplayName);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.pic_User);
+            this.Name = "Account_Control";
+            this.Size = new System.Drawing.Size(1250, 700);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_User)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.TextBox txt_RepeatNewPass;
+        private System.Windows.Forms.TextBox txt_NewPass;
+        private System.Windows.Forms.TextBox txt_CurrentPass;
+        private System.Windows.Forms.TextBox txt_DisplayName;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.PictureBox pic_User;
+    }
+}
---
+++ QuanLyCuaHangGear/View/Welcome_Control.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace QuanLyCuaHangGear
+{
+    partial class Welcome_Control
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_BigLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_BigLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_BigLogo
+            // 
+            this.pic_BigLogo.Image = global::QuanLyCuaHangGear.Properties.Resources.Store_Logo;
+            this.pic_BigLogo.Location = new System.Drawing.Point(375, 88);
+            this.pic_BigLogo.Name = "pic_BigLogo";
+            this.pic_BigLogo.Size = new System.Drawing.Size(500, 500);
+            this.pic_BigLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_BigLogo.TabIndex = 0;
+            this.pic_BigLogo.TabStop = false;
+            // 
+            // Welcome_Control
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.Controls.Add(this.pic_BigLogo);
+            this.Name = "Welcome_Control";
+            this.Size = new System.Drawing.Size(1250, 700);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_BigLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_BigLogo;
+    }
+}
